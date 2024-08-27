640db6a594083a9c6b4d01648b02d6a33cdb7217
Jared Reiss-Lavoie <jared.reiss@comcast.net>
2024-08-27 00:52:08 -0500

    Begin rough sketch idea of homepage

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+def TopBarSection():
+    return Div()
 
+def LeaderboardItem(player_name, points):
+    return Label(f"{player_name}: {points}")
+
+
+def LeaderboardSection():
+    return (Div(
+                H1('Leaderboard'),
+                Div(
+                    LeaderboardItem("Player 1", "0 Points")
+                )
+            ))
+
+hdrs = []
+
+app,rt = fast_app(live=True)
+
+# Homepage
 @rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+def get():
+    title = "Fantasy Soccer Hub"
 
-@rt('/change')
-def get(): return P('Nice to be here!')
+    return (
+            Title(title), 
+            Main(
+                TopBarSection(),
+                LeaderboardSection()
+            )
+            )
 
 serve()
